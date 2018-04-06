8c402fcd72f0848d5f9a8778a3feeea6c35b2419
Mike Dumais <mike.dumais@live.com>
2018-04-06 04:35:54 -0400

    messing with Title Env variable

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const title = process.env.TITLE;
+const appTitle = process.env.TITLE;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title });
+  res.render('index', { appTitle });
 });
 
 module.exports = router;
